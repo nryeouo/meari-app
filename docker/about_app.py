dea395b1ec41b714fbcf52e9d789dcadeb0e35bd
nryeouo <43399370+nryeouo@users.noreply.github.com>
2025-12-23 18:40:39 +0900

    💿 8013–8016

--- docker/about_app.py
+++ docker/about_app.py
@@ -1,6 +1,6 @@
 class aboutApp:
   def __init__(self):
     self.name = "화면반주가상체계《메아리》"
-    self.date = "2025-11-20"
+    self.date = "2025-12-23"
     self.version = self.date
     self.owner = "조선화면반주음악대회 실행위원회"
